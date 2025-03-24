0f52d33bcf1c5ca829c3fc030496195b5c00cc35
ChinenLain <82330723+ChinenLain@users.noreply.github.com>
2025-03-24 21:28:06 +0800

    测试：画一个三角形，Shader类

--- Calyx/src/Platform/OpenGL/OpenGLContext.cpp
+++ Calyx/src/Platform/OpenGL/OpenGLContext.cpp
@@ -18,6 +18,11 @@ namespace Calyx {
         glfwMakeContextCurrent(m_WindowHandle);
         int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
         CLX_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+        CLX_CORE_INFO("OpenGL Info:");
+        CLX_CORE_INFO("  Vendor: {0}", reinterpret_cast<const char*>(glGetString(GL_VENDOR)));
+        CLX_CORE_INFO("  Renderer: {0}", reinterpret_cast<const char*>(glGetString(GL_RENDERER)));
+        CLX_CORE_INFO("  Version: {0}", reinterpret_cast<const char*>(glGetString(GL_VERSION)));
     }
 
     void OpenGLContext::SwapBuffers()
